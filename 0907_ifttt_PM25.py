bbc92c167484a880c1b4d97203516ae92ec3ba77
f8481210 <59558992+f8481210@users.noreply.github.com>
2021-09-07 18:56:53 +0800

    Add files via upload

---
+++ 0907_ifttt_PM25.py
@@ -0,0 +1,41 @@
+import requests
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome = webdriver.Chrome(options=chrome_options)
+chrome.get("https://airtw.epa.gov.tw/")
+time.sleep(3)
+
+#下拉選單選擇台北松山
+selectCounty = Select(chrome.find_element_by_id('ddl_county'))
+selectCounty.select_by_index(1) #找到第2個選項 > 台北市
+time.sleep(1)
+selectSite = Select(chrome.find_element_by_id('ddl_site'))  #測站地點
+selectSite.select_by_index(4)
+time.sleep(1)
+
+#取得測站點、時間、PM2.5值
+soup = BeautifulSoup(chrome.page_source,"html.parser")
+air_info = soup.find_all('div',class_ = 'info')[0]
+state = air_info.find('h4').text[:6]
+date = air_info.find('div',class_ = 'date').text.strip()[:16]
+PM25 = int(air_info.find('span',id = 'PM25').text)
+air_quality = ''
+if PM25<16:
+    air_quality = "good"+' PM2.5 = '+str(PM25)
+elif PM25<35:
+    air_quality = "moderate"+' PM2.5 = '+str(PM25)
+else:
+    air_quality = "Unhealthy"+' PM2.5 = '+str(PM25)
+    
+    
+webhook_key = "填入Webhook金鑰"
+trigger_name = "填入觸發條件名稱"
+url ='https://maker.ifttt.com/trigger/'+trigger_name+'/with/key/'+webhook_key+'?value1={}&value2={}&value3={}'.format(date,state,air_quality)
+requests.get(url)
+print(date,state,air_quality)
